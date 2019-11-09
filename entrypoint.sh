5b9c981a8c76d25f03db447cfffa730cb9632427
Quinn Slack <qslack@qslack.com>
2019-11-08 22:36:38 -0800

    upload raw executable (not just targz)
    Fixes https://github.com/ngs/go-release.action/issues/2.

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,25 +19,31 @@ RELEASE_NAME=$(echo $EVENT_DATA | jq -r .release.tag_name)
 PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
 NAME="${PROJECT_NAME}_${RELEASE_NAME}_${GOOS}_${GOARCH}"
 
-EXT=''
-
-if [ $GOOS == 'windows' ]; then
-  EXT='.exe'
-fi
-
-tar cvfz tmp.tgz $EXECUTABLE_PATH
-CHECKSUM=$(md5sum tmp.tgz | cut -d ' ' -f 1)
-
 curl \
   -X POST \
-  --data-binary @tmp.tgz \
-  -H 'Content-Type: application/gzip' \
+  --data-binary @$EXECUTABLE_PATH \
+  -H 'Content-Type: application/octet-stream' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${NAME}.tar.gz"
-
+  "${UPLOAD_URL}?name=${NAME}${EXT}"
 curl \
   -X POST \
-  --data $CHECKSUM \
+  --data $(md5sum $EXECUTABLE_PATH | cut -d ' ' -f 1) \
   -H 'Content-Type: text/plain' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${NAME}_checksum.txt"
+  "${UPLOAD_URL}?name=${NAME}${EXT}.md5"
+
+tar cvfz tmp.tgz $EXECUTABLE_PATH
+curl \
+	-X POST \
+	--data-binary @tmp.tgz \
+	-H 'Content-Type: application/gzip' \
+	-H "Authorization: Bearer ${GITHUB_TOKEN}" \
+	"${UPLOAD_URL}?name=${NAME}.tgz"
+curl \
+	-X POST \
+	--data $(md5sum tmp.tgz | cut -d ' ' -f 1) \
+	-H 'Content-Type: text/plain' \
+	-H "Authorization: Bearer ${GITHUB_TOKEN}" \
+	"${UPLOAD_URL}?name=${NAME}.tgz.md5"
+
+rm -f $EXECUTABLE_PATH tmp.tgz
